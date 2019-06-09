2ebcf988b7a5b32eb1a64d4390e27f232134f718
Juuliien <36441596+Juuliien@users.noreply.github.com>
2019-06-09 12:11:25 +0200

    Add files via upload

--- Controleur.java
+++ Controleur.java
@@ -3,22 +3,18 @@ package controleur;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import modele.Bulletin;
-import modele.Connexion;
-import modele.Personne;
-import modele.UnBulletin;
+
+import model.Bulletin;
+import model.Connexion;
+import model.Personne;
+import model.UnBulletin;
 
 public class Controleur {
-        /**
-         * Constructeur par défaut 
-         */
+
 	public Controleur() {
 		
 	}
-	/**
-         * Recherche un élève
-         * @return une liste de type Persone 
-         */
+	
 	public List<Personne> recherchePersonne(){
 		ArrayList<Personne> personneList = new ArrayList<Personne>();
 		Connection conn = Connexion.getConnexion();
@@ -63,10 +59,7 @@ public class Controleur {
 		
 		return personneList;
 	}
-	/**
-         * Recherche un enseignant
-         * @return  une liste de type Personne
-         */
+	
 	public List<Personne> recherchePersonneEn(){
 		ArrayList<Personne> personneList = new ArrayList<Personne>();
 		Connection conn = Connexion.getConnexion();
@@ -111,10 +104,7 @@ public class Controleur {
 		
 		return personneList;
 	}
-	/**
-         * Retourne la classe de l'élève
-         * @return une ArrayList de type String
-         */
+	
 	public ArrayList<String> obtenirNiveau(){
 		ArrayList<String> nivListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -144,11 +134,7 @@ public class Controleur {
 		
 		return nivListe;
 	}
-	/**
-         * Retourne le numéro de la classe de l'élve
-         * @param niveau 
-         * @return 
-         */
+	
 	public ArrayList<String> obtenirClasse(String niveau){
 		ArrayList<String> classeListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -178,13 +164,7 @@ public class Controleur {
 		
 		return classeListe;
 	}
-	/**
-         * Ajoute une personne
-         * @param nom nom de l'élève
-         * @param prenom prénom de l'élève
-         * @param type checkbox élève ou enseignant
-         * @return ID de la personne
-         */
+	
 	public int ajouterPersonne(String nom, String prenom, String type) {
 		Connection conn = Connexion.getConnexion();
 		int idPersonne = -1;
@@ -219,11 +199,7 @@ public class Controleur {
 		}
 		return idPersonne;
 	}
-	/**
-         * Inscription d'un élève
-         * @param idPersonne id de la personne avec la fonction ajouterPersonne
-         * @param idClasse id de classe avec la fonction obtenir Idclass
-         */
+	
 	public void ajoutInscription(int idPersonne, int idClasse) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -247,11 +223,7 @@ public class Controleur {
 			}
 		}	
 	}
-	/**
-         * Obtiens l'id de la classe 
-         * @param classe
-         * @return un int avec l'id de la classe
-         */
+	
 	public int obtenirIdClasse(String classe){
 		Connection conn = Connexion.getConnexion();
 		int idClasse = -1;
@@ -281,10 +253,7 @@ public class Controleur {
 		
 		return idClasse;
 	}
-	/**
-         * Retourne un tableau avec les différentes matières
-         * @return une ArrayList avec les matières
-         */
+	
 	public ArrayList<String> obtenirDispline(){
 		ArrayList<String> disciplineListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -314,10 +283,7 @@ public class Controleur {
 		
 		return disciplineListe;
 	}
-	/**
-         * Retourne toute les classes dans un tableau 
-         * @return retourne un ArrayLisst de String 
-         */
+	
 	public ArrayList<String> obtenirToutClasse(){
 		ArrayList<String> classeListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -347,11 +313,7 @@ public class Controleur {
 		
 		return classeListe;
 	}
-	/**
-         * Obtiens l'ID de la discipline
-         * @param discipline
-         * @return un entier qui correspond à l'id
-         */
+	
 	public int obtenirIdDiscipline(String discipline){
 		Connection conn = Connexion.getConnexion();
 		int idDis = -1;
@@ -381,12 +343,7 @@ public class Controleur {
 		
 		return idDis;
 	}
-	/**
-         * Ajouter un enseignement
-         * @param idDis id de la discipline 
-         * @param idPersonne id de la personne 
-         * @param idClasse id de la classe 
-         */
+	
 	public void ajoutEnseignement(int idDis, int idPersonne, int idClasse) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -411,10 +368,7 @@ public class Controleur {
 			}
 		}	
 	}
-	/**
-         * Supprime un élève
-         * @param id id de l'élève
-         */
+	
 	public void supprimerEleve(String id) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -435,10 +389,7 @@ public class Controleur {
 			}
 		}
 	}
-	/**
-         * Supprimer un enseignant
-         * @param id id de l'enseignant
-         */
+	
 	public void supprimerEnseignant(String id) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -459,11 +410,7 @@ public class Controleur {
 			}
 		}
 	}
-	/**
-         * Obtiens le nom d'une personne 
-         * @param id id de la personne 
-         * @return un String avec le nom 
-         */
+	
 	public String obtenirNomPersonne(String id){
 		Connection conn = Connexion.getConnexion();
 		String nom = null;
@@ -493,11 +440,7 @@ public class Controleur {
 		
 		return nom;
 	}
-	/**
-         * Obtiens le prénom d'une personne 
-         * @param id id de la personne 
-         * @return un String avec le prénom
-         */
+	
 	public String obtenirPrenomPersonne(String id){
 		Connection conn = Connexion.getConnexion();
 		String prenom = null;
@@ -527,13 +470,7 @@ public class Controleur {
 		
 		return prenom;
 	}
-	/**
-         * Modifie les attributs d'une personne 
-         * @param nom
-         * @param prenom
-         * @param type
-         * @param id 
-         */
+	
 	public void modifierPersonne(String nom, String prenom, String type, String id) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -562,11 +499,7 @@ public class Controleur {
 			}
 		}
 	}
-	/**
-         * Modifie l'inscription d'une personne
-         * @param idPersonne id personne 
-         * @param idClasse id de classe
-         */
+	
 	public void modifierInscription(String idPersonne, int idClasse) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -589,12 +522,7 @@ public class Controleur {
 			}
 		}	
 	}
-	/**
-         * Modifie les attributs d'un enseignant
-         * @param idDis id de sa discipline
-         * @param idPersonne
-         * @param idClasse 
-         */
+	
 	public void modifierEnseignement(int idDis, String idPersonne, int idClasse) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -618,10 +546,7 @@ public class Controleur {
 			}
 		}	
 	}
-	/**
-         * Méthode de recherche du bulletin d'un élève 
-         * @return un List de type Bulletin 
-         */
+	
 	public List<Bulletin> rechercheBulletin(){
 		ArrayList<Bulletin> bulletinList = new ArrayList<Bulletin>();
 		Connection conn = Connexion.getConnexion();
@@ -657,11 +582,7 @@ public class Controleur {
 		
 		return bulletinList;
 	}
-	/**
-         * Retourne le Trimestre 
-         * @param annee
-         * @return une ArrayList de type String
-         */
+	
 	public ArrayList<String> obtenirTrimestre(int annee){
 		ArrayList<String> trimestreListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -691,10 +612,7 @@ public class Controleur {
 		
 		return trimestreListe;
 	}
-	/**
-         * Retourne l'année scolaire
-         * @return une ArrayList de type String 
-         */
+	
 	public ArrayList<String> obtenirAnnee(){
 		ArrayList<String> anneeListe = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -724,11 +642,7 @@ public class Controleur {
 		
 		return anneeListe;
 	}
-	/**
-         * Recher un bulletin 
-         * @param idBulletin id du bulletin 
-         * @return une List de type UnBulletin
-         */
+	
 	public List<UnBulletin> rechercheUnBulletin(int idBulletin){
 		ArrayList<UnBulletin> bulletinList = new ArrayList<UnBulletin>();
 		Connection conn = Connexion.getConnexion();
@@ -764,10 +678,7 @@ public class Controleur {
 		
 		return bulletinList;
 	}
-	/**
-         * Retourne toutes les matières 
-         * @return ArrayList de type String
-         */
+	
 	public ArrayList<String> obtenirMatiere(){
 		ArrayList<String> matiereList = new ArrayList<String>();
 		Connection conn = Connexion.getConnexion();
@@ -797,12 +708,7 @@ public class Controleur {
 		
 		return matiereList;
 	}
-	/**
-         * Vérifie l'existance et retourne l'id du bulletin
-         * @param idBulletin id du bulletin 
-         * @param idEnseignement id de l'enseignant
-         * @return id du bulletin si existe , -1 sinon
-         */
+	
 	public int verifierExistanceBulletinMat(int idBulletin, String idEnseignement) {
 		int estPresent = -1;
 		
@@ -832,12 +738,7 @@ public class Controleur {
 		
 		return estPresent;
 	}
-	/**
-         * Ajouter une interrogation 
-         * @param idbulletinMat id du bulletin 
-         * @param note note de l'évaluation 
-         * @param appre appréciation 
-         */
+	
 	public void ajoutEval(int idbulletinMat, int note, String appre) {
 		Connection conn = Connexion.getConnexion();
 		
@@ -862,12 +763,7 @@ public class Controleur {
 			}
 		}	
 	}
-	/**
-         * Ajoute une matière dans un bulletin 
-         * @param idBulletin
-         * @param idEnseignement
-         * @return l'id de la matière créée
-         */
+	
 	public int ajoutBulletinMat(int idBulletin, int idEnseignement) {
 		Connection conn = Connexion.getConnexion();
 		int resultat = -1;
@@ -905,6 +801,95 @@ public class Controleur {
 	
 	}
 	
+	public ArrayList<String> obtenirEleveInscrit(){
+		ArrayList<String> eleveList = new ArrayList<String>();
+		Connection conn = Connexion.getConnexion();
+		
+		try
+		{
+			String requete = new String("SELECT inscription.id, nom, prenom FROM inscription join personne on inscription.personne_id = personne.id");
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(requete);
+
+			while ( rs.next() )
+			{
+				String f = rs.getInt("id")+" - "+rs.getString("nom")+" - "+rs.getString("prenom");
+				eleveList.add(f);
+			}
+		}
+		catch (SQLException ex3)
+		{
+			while (ex3 != null)
+			{
+				System.out.println(ex3.getSQLState());
+				System.out.println(ex3.getMessage());
+				System.out.println(ex3.getErrorCode());
+				ex3=ex3.getNextException();
+			}
+		}
+		
+		return eleveList;
+	}
+	/**
+	 * 
+	 * @param annee
+	 * @param numeroTrimestre
+	 * @return un int
+	 */
+	public int obtenirIdTrimestre(String annee, String numeroTrimestre) {
+		int id = -1;
+		Connection conn = Connexion.getConnexion();
+		
+		try
+		{
+			String requete = new String("SELECT id FROM trismestre WHERE anneescolaire_id="+annee+" AND numero="+numeroTrimestre);
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(requete);
+
+			while ( rs.next() )
+			{
+				id = rs.getInt("id");
+			}
+		}
+		catch (SQLException ex3)
+		{
+			while (ex3 != null)
+			{
+				System.out.println(ex3.getSQLState());
+				System.out.println(ex3.getMessage());
+				System.out.println(ex3.getErrorCode());
+				ex3=ex3.getNextException();
+			}
+		}
+		
+		return id;
+	}
+	
+	public void CreerBulletin(int idTrimestre, String idInscription) {
+Connection conn = Connexion.getConnexion();
+		
+		try
+		{
+			String requete = new String("INSERT INTO bulletin (trimestre_id, inscription_id) VALUES (?,?);");
+			PreparedStatement statement = conn.prepareStatement(requete);
+			
+			int stoi = Integer.parseInt(idInscription);
+			statement.setInt(1, idTrimestre);
+			statement.setInt(2, stoi);
+			
+			statement.executeUpdate();
+		}
+		catch (SQLException ex3)
+		{
+			while (ex3 != null)
+			{
+				System.out.println(ex3.getSQLState());
+				System.out.println(ex3.getMessage());
+				System.out.println(ex3.getErrorCode());
+				ex3=ex3.getNextException();
+			}
+		}
+	}
 	
 	
 }
